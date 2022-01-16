95e660ba5427ac0b5224b0865cd84a446d0cc24f
Ahmed Basha <dev@ahmedbasha.com>
2022-01-16 18:23:47 +0900

    Implement initial UI for RootView

--- Shared/RootView.swift
+++ Shared/RootView.swift
@@ -13,7 +13,55 @@ struct RootView: View {
 	let conversionRatesStore: Store<ConversionRatesState, ConversionRatesAction>
 
 	var body: some View {
-		Text("Hello, world!")
+		NavigationView {
+			WithViewStore(conversionRatesStore) { conversionRatesViewStore in
+				Group {
+					if conversionRatesViewStore.rates != nil {
+						Text("ConvertRateView")
+							.overlay {
+								if conversionRatesViewStore.isFetching {
+									ProgressView(R.string.localizable.loadingConversionRates())
+										.padding()
+										.frame(minWidth: 150, minHeight: 150)
+										.background(.thinMaterial)
+										.clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+								}
+							}
+					} else if conversionRatesViewStore.isFetching {
+						ProgressView(R.string.localizable.loadingConversionRates())
+					} else {
+						VStack(spacing: 16) {
+							Text(R.string.localizable.rootViewErrorTitle())
+								.font(.headline)
+							Text(R.string.localizable.rootViewErrorMessage())
+								.font(.caption)
+						}
+						.multilineTextAlignment(.center)
+						.padding(.horizontal, 24)
+					}
+				}
+				.task {
+					// check persisted data
+					conversionRatesViewStore.send(.dataPersistenceAction(.setFromPersistedDataIfNil))
+					// initiate a fetch request
+					conversionRatesViewStore.send(.fetchConversionRates)
+				}
+				.toolbar {
+					ToolbarItem(placement: ToolbarItemPlacement.navigationBarTrailing) {
+						Button {
+							conversionRatesViewStore.send(.fetchConversionRates)
+						} label: {
+							Image(systemName: "arrow.clockwise")
+								.frame(minWidth: 44, minHeight: 44)
+								.contentShape(Rectangle())
+						}
+					}
+				}
+				.alert(conversionRatesStore.scope(state: \.alert), dismiss: ConversionRatesAction.dismissAlert)
+			}
+			.navigationBarTitle(R.string.localizable.rootViewNavigationTitle())
+			.navigationBarTitleDisplayMode(.inline)
+		}
 	}
 }
 
